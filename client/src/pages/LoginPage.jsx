7d9cbacc60bf98668fc9ea90134bcfba069e963f
Rahul <rahulsangwan239@gmail.com>
2025-07-06 21:54:18 +0530

    basic setup and register

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "../styles/Login.scss";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit=() => {
+    e.preventDefault()
+
+    try{
+      const response= await fetch ("https://localhost:3001/auth/login", {
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify({email,password})
+      })
+
+      const loggedIn=await response.json()
+    } catch(err){
+
+    }
+  }
+
+  return (
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => e.target.value}
+            required
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => e.target.value}
+            required
+          ></input>
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In Here</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
