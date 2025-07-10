59dba9ea28adc3b40b4966dd63c2abf8b8685442
Richa <gargricha1209@gmail.com>
2025-07-10 20:27:49 +0530

    basic structure of createListing

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,13 +1,180 @@
-import React from 'react'
-import '../styles/CreateListing.scss'
-import Navbar from '../components/Navbar'
+  import "../styles/CreateListing.scss";
+import Navbar from "../components/Navbar";
+import { categories, types } from "../data";
+import { RemoveCircleOutline, AddCircleOutline } from "@mui/icons-material";
+import variables from "../styles/variables.scss";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const CreateListing = () => {
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("");
+
+  const [formLocation, setFormLocation] = useState({
+    streetAddress: "",
+    aptSuite: "",
+    city: "",
+    province: "",
+    country: "",
+  });
+
+  const [guestCount, setGuestCount] = useState(1);
+  const [bedroomCount, setBedroomCount] = useState(1);
+  const [bedCount, setBedCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
+
+  const handleChangeLocation = (e) => {
+    const { name, value } = e.target;
+    setFormLocation({ ...formLocation, [name]: value });
+  };
+
   return (
     <>
-    <Navbar> </Navbar>
-    </>
-  )
-}
+      <Navbar />
+      <div className="create-listing">
+        <h1>Publish Your Place</h1>
+        <form>
+          <div className="create-listing_step1">
+            <h2>Step 1: Tell us about your place</h2>
+            <hr />
+            <h3>Which of these categories best describes your place?</h3>
+            <div className="category-list">
+              {categories.map((item, index) => (
+                <div
+                  className={`category ${
+                    category === item.label ? "selected" : ""
+                  }`}
+                  key={index}
+                  onClick={() => setCategory(item.label)}
+                >
+                  <div className="category_icon">{item.icon}</div>
+                  <p>{item.label}</p>
+                </div>
+              ))}
+            </div>
 
-export default CreateListing
+            <h3>What type of place will guests have?</h3>
+            <div className="type-list">
+              {types.map((item, index) => (
+                <div
+                  className={`type ${type === item.name ? "selected" : ""}`}
+                  key={index}
+                  onClick={() => setType(item.name)}
+                >
+                  <div className="type_text">
+                    <h4>{item.name}</h4>
+                    <p>{item.description}</p>
+                  </div>
+                  <div className="type_icon">{item.icon}</div>
+                </div>
+              ))}
+            </div>
+
+            <h3>Where's your place located?</h3>
+            <div className="full">
+              <div className="location">
+                <p>Street Address</p>
+                <input
+                  type="text"
+                  placeholder="Street Address"
+                  name="streetAddress"
+                  value={formLocation.streetAddress}
+                  onChange={handleChangeLocation}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="half">
+              <div className="location">
+                <p>Apartment, Suite, etc. (if applicable)</p>
+                <input
+                  type="text"
+                  placeholder="Apt, Suite, etc."
+                  name="aptSuite"
+                  value={formLocation.aptSuite}
+                  onChange={handleChangeLocation}
+                  required
+                />
+              </div>
+              <div className="location">
+                <p>City</p>
+                <input
+                  type="text"
+                  placeholder="City"
+                  name="city"
+                  value={formLocation.city}
+                  onChange={handleChangeLocation}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="half">
+              <div className="location">
+                <p>Province</p>
+                <input
+                  type="text"
+                  placeholder="Province"
+                  name="province"
+                  value={formLocation.province}
+                  onChange={handleChangeLocation}
+                  required
+                />
+              </div>
+              <div className="location">
+                <p>Country</p>
+                <input
+                  type="text"
+                  placeholder="Country"
+                  name="country"
+                  value={formLocation.country}
+                  onChange={handleChangeLocation}
+                  required
+                />
+              </div>
+            </div>
+
+            <h3>Share some basics about your place</h3>
+            <div className="basics">
+              {[
+                { label: "Guests", count: guestCount, setCount: setGuestCount },
+                { label: "Bedrooms", count: bedroomCount, setCount: setBedroomCount },
+                { label: "Beds", count: bedCount, setCount: setBedCount },
+                { label: "Bathrooms", count: bathroomCount, setCount: setBathroomCount },
+              ].map((item, index) => (
+                <div className="basic" key={index}>
+                  <p>{item.label}</p>
+                  <div className="basic_count">
+                    <RemoveCircleOutline
+                      onClick={() => item.count > 1 && item.setCount(item.count - 1)}
+                      sx={{
+                        fontSize: "25px",
+                        cursor: "pointer",
+                        "&:hover": { color: variables.pinkred },
+                      }}
+                    />
+                    <p>{item.count}</p>
+                    <AddCircleOutline
+                      onClick={() => item.setCount(item.count + 1)}
+                      sx={{
+                        fontSize: "25px",
+                        cursor: "pointer",
+                        "&:hover": { color: variables.pinkred },
+                      }}
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default CreateListing;
